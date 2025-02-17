1d686ea8695d90fe18496c171d485f6c182e1db9
PhamGiaThinh03 <phamgiathinh973@gmail.com>
2025-02-17 18:55:37 +0700

    Submit progress_test assignment coursera 1

--- download.py
+++
@@ -1,4 +0,0 @@
-import pdfkit
-
-url = "https://www.coursera.org/learn/fundamentals-of-reinforcement-learning/assignment-submission/lMCZf/sequential-decision-making/view-feedback"  # Thay bằng trang web bạn cần lưu
-pdfkit.from_url(url, "output.pdf")
